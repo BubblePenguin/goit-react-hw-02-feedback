0b80eb041112ff44f6e6d40a6acf0ac0e0622430
BubblePenguin <kotov.senpai@gmail.com>
2023-07-02 22:01:55 +0300

    upd to hooks

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,46 +1,87 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Buttons } from './FeedButtons';
 import { Statistics } from './Statistics';
-class Feedback extends Component {
-  static defaultProps = {};
+// class Feedback extends Component {
+//   static defaultProps = {};
 
-  state = { good: 0, mid: 0, bad: 0 };
+//   state = { good: 0, mid: 0, bad: 0 };
 
-  onLeaveFeedback = e => {
+//   onLeaveFeedback = e => {
+//     const { name } = e.target;
+//     this.setState(ps => ({
+//       [name]: ps[name] + 1,
+//     }));
+//   };
+
+//   countTotalFeedback(ps = this.state) {
+//     return Object.values(ps).reduce((x, c) => x + c, 0);
+//   }
+
+//   countPositiveFeedbackPercentage(ps = this.state) {
+//     return ((ps.good / this.countTotalFeedback(ps)) * 100 || 0).toFixed(0);
+//   }
+//   render() {
+//     return (
+//       <div
+//         style={{
+//           margin: 100,
+//         }}
+//       >
+//         <Buttons onLeaveFeedback={this.onLeaveFeedback} />
+//         <Statistics
+//           total={this.countTotalFeedback()}
+//           positivePercentage={this.countPositiveFeedbackPercentage()}
+//           {...this.state}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+export default function Feedback() {
+  const [good, setGood] = useState(0);
+  const [mid, setMid] = useState(0);
+  const [bad, setBad] = useState(0);
+  const onLeaveFeedback = e => {
     const { name } = e.target;
-    this.setState(ps => ({
-      [name]: ps[name] + 1,
-    }));
 
-    // console.log(this.state);
-    // console.log(this.countTotalFeedback());
-    // console.log(this.countPositiveFeedbackPercentage());
+    switch (name) {
+      case 'mid':
+        setMid(mid + 1);
+        break;
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  countTotalFeedback(ps = this.state) {
+  const vars = { good, mid, bad };
+
+  const countTotalFeedback = (ps = vars) => {
     return Object.values(ps).reduce((x, c) => x + c, 0);
-  }
+  };
 
-  countPositiveFeedbackPercentage(ps = this.state) {
-    return ((ps.good / this.countTotalFeedback(ps)) * 100 || 0).toFixed(0);
-  }
-  render() {
-    //const buttons = {};
-    return (
-      <div
-        style={{
-          margin: 100,
-        }}
-      >
-        <Buttons onLeaveFeedback={this.onLeaveFeedback} />
-        <Statistics
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-          {...this.state}
-        />
-      </div>
-    );
-  }
+  const countPositiveFeedbackPercentage = (ps = vars) => {
+    return ((ps.good / countTotalFeedback(ps)) * 100 || 0).toFixed(0);
+  };
+
+  return (
+    <div
+      style={{
+        margin: 100,
+      }}
+    >
+      <Buttons onLeaveFeedback={onLeaveFeedback} />
+      <Statistics
+        total={countTotalFeedback()}
+        positivePercentage={countPositiveFeedbackPercentage()}
+        {...vars}
+      />
+    </div>
+  );
 }
-
-export default Feedback;
